00b2bf1ab7c1b959000a9e3014c95d977ff13e2b
Reyna Ramirez <ramirezverareyna1@gmail.com>
2021-09-17 12:19:37 -0500

    friendlist-working

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -6,6 +6,9 @@ import ThoughtList from '../components/ThoughtList';
 import { useQuery } from '@apollo/client';
 import { QUERY_USER } from '../utils/queries';
 
+import FriendList from '../components/FriendList';
+
+
 const Profile = () => {
   const { username: userParam } = useParams();
 
@@ -31,6 +34,16 @@ const Profile = () => {
         <div className="col-12 mb-3 col-lg-8">
           <ThoughtList thoughts={user.thoughts} title={`${user.username}'s thoughts...`} />
         </div>
+
+        <div className="col-12 col-lg-3 mb-3">
+          <FriendList
+            username={user.username}
+            friendCount={user.friendCount}
+            friends={user.friends}
+          />
+        </div>
+
+
       </div>
     </div>
   );
