a460472a621fcb2895faab005e3288f1ecbd2ed6
Reyna Ramirez <ramirezverareyna1@gmail.com>
2021-09-17 20:12:53 -0500

    login-working

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
+import { useMutation } from '@apollo/client';
+import { LOGIN_USER } from '../utils/mutations';
 
 const Login = (props) => {
   const [formState, setFormState] = useState({ email: '', password: '' });
+  const [login, { error }] = useMutation(LOGIN_USER);
 
   // update state based on form input changes
   const handleChange = (event) => {
@@ -13,16 +16,20 @@ const Login = (props) => {
     });
   };
 
-  // submit form
-  const handleFormSubmit = async (event) => {
-    event.preventDefault();
+// submit form
+const handleFormSubmit = async event => {
+  event.preventDefault();
 
-    // clear form values
-    setFormState({
-      email: '',
-      password: '',
+  try {
+    const { data } = await login({
+      variables: { ...formState }
     });
-  };
+
+    console.log(data);
+  } catch (e) {
+    console.error(e);
+  }
+};
 
   return (
     <main className='flex-row justify-center mb-4'>
@@ -52,6 +59,7 @@ const Login = (props) => {
               <button className='btn d-block w-100' type='submit'>
                 Submit
               </button>
+              {error && <div>Login failed</div>}
             </form>
           </div>
         </div>
